b9cb5da36b8db0f57801da5b06dddec4979c0411
mmtakeuchi <mikemtaki@gmail.com>
2021-01-28 16:17:10 -0700

    commit tree 121d3ea49fa50aa11e9dd36f7258b94e19b73a2d

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,20 @@
 # Implement your procedural solution here!
+def largest_palindrome_product
+    largest = 0
+
+    (100..999).each do |i|
+        (100..999).each do |j|
+            product = i * j
+
+            if (is_palindrome(product) && product > largest)
+                largest = product
+            end
+        end
+    end
+
+    largest
+end
+
+def is_palindrome(input)
+    input.to_s == input.to_s.reverse
+end
